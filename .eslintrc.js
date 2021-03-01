78bf9467e19809f02656816e9954dd26cfce1125
oksang <dev.oksang@gmail.com>
2021-03-01 14:06:59 +0900

    eslint 룰 추가
    react prop-types 규칙 끔

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,6 @@ module.exports = {
   rules: {
     "react/react-in-jsx-scope": "off",
     "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/prop-types": "off",
   },
 };
