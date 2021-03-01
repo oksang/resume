05bbdb64466d5355987f0f115f9470b2c50ea96f
oksang <dev.oksang@gmail.com>
2021-03-01 11:56:38 +0900

    마크업 완료

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,87 @@
-import styles from '../styles.module.css';
-
 export default function Resume() {
   return (
-    <div className={styles.hello}>
-      <p>Hello World</p>
-    </div>
+    <article>
+      <section>
+        <h1>엄일경</h1>
+        <sub>Last updated: 2021.02.25</sub>
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <strong>Github</strong>
+              </td>
+              <td>
+                <a href="https://github.com/oksang">
+                  https://github.com/oksang
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Blog</strong>
+              </td>
+              <td>
+                <a href="https://github.com/oksang">https://oksang.dev</a>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <strong>Mail</strong>
+              </td>
+              <td>
+                <a href="mailto:aumilkyeong@gmail.com">aumilkyeong@gmail.com</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+      <section>
+        <h1>Experience</h1>
+        <h2>Marketit</h2>
+        <table>
+          <tbody>
+            <tr>
+              <td>기간</td>
+              <td>16.10 ~ 20.03</td>
+            </tr>
+            <tr>
+              <td>직무</td>
+              <td>웹개발자, 매니저</td>
+            </tr>
+            <tr>
+              <td>프로젝트</td>
+              <td>Marketit AD, Marketit API, M.Shop</td>
+            </tr>
+          </tbody>
+        </table>
+        <h3>Marketit AD</h3>
+        <p>인플루언서 마케팅 광고주 플랫폼</p>
+        <ul>
+          <li>16.10 ~ 19.09</li>
+          <li>angularJS, ES5, Bootstrap을 활용한 UI 개발</li>
+        </ul>
+        <h3>Marketit API</h3>
+        <p>인플루언서 및 광고주 플랫폼, 백오피스에서 사용하는 API</p>
+        <ul>
+          <li>16.10 ~ 19.09</li>
+          <li>
+            php를 활용한 서드파티(결제, 배송추적, sms) 서비스 통합 기능, 데이터
+            CRUD 기능 개발
+          </li>
+          <li>mysql 쿼리 최적화 및 인덱싱</li>
+        </ul>
+        <h3>M.Shop</h3>
+        <p>인플루언서 공동구매 서비스</p>
+        <ul>
+          <li>16.10 ~ 19.09</li>
+          <li>vue.js, ES5, Bootstrap을 활용한 UI 개발</li>
+        </ul>
+      </section>
+      <section>
+        <h1>Education</h1>
+        <h2>Codesoom</h2>
+        <p>20.06 ~ 20.09 소프트웨어 개발의 지혜 리액트편</p>
+      </section>
+    </article>
   );
 }
